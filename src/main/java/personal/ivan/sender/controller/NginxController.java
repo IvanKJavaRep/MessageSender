29ff6ded8dd2966281601ad8330d38a22ccf9cf4
Харитонин Иван <i.haritonin@crpt.ru>
2023-01-18 17:42:31 +0300

    nginx test

---
+++ src/main/java/personal/ivan/sender/controller/NginxController.java
@@ -0,0 +1,21 @@
+package personal.ivan.sender.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import personal.ivan.sender.entity.MyTableEntity;
+import personal.ivan.sender.feignClient.MyClient;
+
+@RestController
+@RequestMapping("/")
+public class NginxController {
+
+
+    @GetMapping()
+    @ResponseBody
+    public String get() {
+        return "server on port 4028 is listening";
+    }
+}
